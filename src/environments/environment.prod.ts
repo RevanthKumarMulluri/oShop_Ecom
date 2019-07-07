f15bc92eb1bcb5a1280b64ab06fb412aeb01ed75
CS372Pro <cs372pro@gmail.com>
2019-07-07 14:18:34 -0600

    Angular

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyCRYh8PSCYQm0P5_qGjPvMo3x5Ng9Y41tM",
+  authDomain: "oshop-5a766.firebaseapp.com",
+  databaseURL: "https://oshop-5a766.firebaseio.com",
+  projectId: "oshop-5a766",
+  storageBucket: "",
+  messagingSenderId: "662119837862",
+  appId: "1:662119837862:web:d4652c7956804fcd"
+
+  }
 };
