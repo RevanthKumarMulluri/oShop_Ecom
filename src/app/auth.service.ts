d93b9ec21c715d68155832b704b4ea0d644fe0ef
CS372Pro <cs372pro@gmail.com>
2019-07-11 19:33:09 -0600

    To get authorization

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -2,6 +2,11 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase';
 import { Observable } from 'rxjs/Observable';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { ActivatedRoute } from '@angular/router';
+import { AppUser } from './models/app-user';
+import { UserService } from './user.service';
+import 'rxjs/add/observable/empty' ;
 
 @Injectable({
   providedIn: 'root'
@@ -9,15 +14,23 @@ import { Observable } from 'rxjs/Observable';
 export class AuthService {
 
   user$ : Observable <firebase.User>;
-  constructor(private afAuth : AngularFireAuth) { 
+  private userExist : boolean;
+  constructor(private afAuth : AngularFireAuth, private route:ActivatedRoute, private userService : UserService) { 
     this.user$=afAuth.authState;
   }
 
   login(){
+      let returnUrl = this.route.snapshot.queryParamMap.get('returnUrl')||'/';
+      localStorage.setItem('returnUrl',returnUrl);
+      
     this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
   }
 
   logout(){
     this.afAuth.auth.signOut();
   }
+
+ 
+  
+  
 }
