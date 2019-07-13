9eabd6288561b6a124bfed5a45de0edeeff26fba
Revanth <revanth54@gmail.com>
2019-07-13 15:37:29 -0600

    To get list of products

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-admin-products',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminProductsComponent implements OnInit {
 
-  constructor() { }
+  products$;
+
+  constructor(private productService : ProductService) { 
+
+    this.products$=this.productService.getproducts();
+  }
 
   ngOnInit() {
   }
 
+    
 }
