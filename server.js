9563359aa436c07773b5bacf9303a348bfcf8334
CS372Pro <cs372pro@gmail.com>
2019-07-12 20:44:31 -0600

    initial heroku commit

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/oshop'));
+app.use(express.static(__dirname + '/dist'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/oshop/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 // default Heroku port
 app.listen(process.env.PORT || 5000);
+console.log('console listening');
