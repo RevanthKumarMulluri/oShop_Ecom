d7547ade8d22cfe85587082ef0e72788c5fd41ff
Revanth <revanth54@gmail.com>
2019-07-22 19:10:01 -0600

    added count badge to display number of items in shopping cart

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -4,18 +4,27 @@ import * as firebase from 'firebase';
 import { Observable } from 'rxjs/Observable';
 import { AuthService } from '../auth.service';
 import { AppUser } from '../models/app-user';
+import { ShoppingCartService } from '../services/shopping-cart.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent {
-
+export class BsNavbarComponent implements OnInit {
+cartItemCount : number;
  appUser : AppUser;
-  constructor(private auth : AuthService) { 
-   auth.appUser$.subscribe(appUser=>this.appUser=appUser);
+ 
+  constructor(private auth : AuthService, private cartService : ShoppingCartService) { }
 
+  async ngOnInit(){
+    
+    this.auth.appUser$.subscribe(appUser=>this.appUser=appUser);
+    let cart$ = await this.cartService.getCart();
+    cart$.subscribe(cart => {
+      this.cartItemCount = this.cartService.getItemQuantity(cart.items);
+    });
   }
 
   logout(){
