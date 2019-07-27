329dbabb093778dac04211f1ca6442a80973d4bf
Revanth <revanth54@gmail.com>
2019-07-27 17:31:56 -0600

    Shows the all the orders has been customer been placed

--- src/app/my-orders/my-orders.component.ts
+++ src/app/my-orders/my-orders.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { OrderService } from '../services/order.service';
 
 @Component({
-  selector: 'app-my-orders',
+  selector: 'my-orders',
   templateUrl: './my-orders.component.html',
   styleUrls: ['./my-orders.component.css']
 })
-export class MyOrdersComponent implements OnInit {
+export class MyOrdersComponent {
+ 
+   orders$;
+  constructor(private authService: AuthService,
+    private orderService: OrderService) { 
+      this.orders$= authService.user$.switchMap( u => orderService.getOrdersByUser(u.uid));
+    }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+ 
 
 }
